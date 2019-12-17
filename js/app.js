f351b0c69369a4801b7df437a8c73b0a34632cd7
marlon-morrison <52177393+marlon-morrison@users.noreply.github.com>
2019-12-17 09:46:12 -0600

    new operations

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ var nums = document.getElementsByClassName("number");
 var operators = document.getElementsByClassName("operations");
 var equals = document.getElementById("equals")
 var output = document.getElementById("output")
-
+//operators
 for (let i = 0; i < operators.length; i++) {
     operators[i].addEventListener("click",function () {
         oldNum = theNum;
@@ -14,17 +14,18 @@ for (let i = 0; i < operators.length; i++) {
 
     });
 };
-
+//numbers
 for (let i = 0; i < nums.length; i++) {
     nums[i].addEventListener("click",function () {
         theNum += this.getAttribute("data-value");
         result = "";
+        debugger
         output.innerHTML = theNum; // Display current number
         console.log(theNum);
 
     });
 };
-
+//all clear
 var allClear = document.querySelectorAll(".ac")
 for (let i = 0; i < allClear.length; i++) {
     allClear[i].addEventListener("click",function () {
@@ -38,36 +39,37 @@ for (let i = 0; i < allClear.length; i++) {
     });
     
 }; 
-
+//calculation
 var displayNum = function() {
-      oldNum = parseFloat(oldNum);
-      theNum = parseFloat(theNum);
+  oldNum = parseFloat(oldNum);
+  theNum = parseFloat(theNum);
   
-      switch (operators) {
-        case "+":
-          resultNum = oldNum + theNum;
-          break;
-  
-        case "-":
-          resultNum = oldNum - theNum;
-          break;
-  
-        case "x":
-          resultNum = oldNum * theNum;
-          break;
-  
-        case "/":
-          resultNum = oldNum / theNum;
-          break;
-  
-        default:
-          resultNum = theNum;
-      }
-  
-      // Display result
-      output.innerHTML = resultNum;
-  
-      //Reset oldNum & keep result
-      oldNum = 0;
-      theNum = resultNum;
-    };
+  switch (operators) {
+    case "+":
+      resultNum = oldNum + theNum;
+      break;
+
+    case "-":
+      resultNum = oldNum - theNum;
+      break;
+
+    case "x":
+      resultNum = oldNum * theNum;
+      break;
+
+    case "/":
+      resultNum = oldNum / theNum;
+      break;
+    case "%":
+      resultNum = (oldNum / 100).toFixed(2);
+      break;
+
+    default:
+      resultNum = theNum;
+  }
+  // Display result
+  output.innerHTML = resultNum;
+  //Reset oldNum & keep result
+  oldNum = 0;
+  theNum = resultNum;
+};
